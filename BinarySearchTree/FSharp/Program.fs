04fb735a71afaeb9796f74527ab4cdfd9de7e26d
Andrew Walker <alwalker21@gmail.com>
2012-11-05 21:56:31 -0600

    Started F# binary search tree.

---
+++ BinarySearchTree/FSharp/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+module Program
+
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
---
+++ BinarySearchTree/FSharp/Tree.fs
@@ -0,0 +1,24 @@
+﻿module Tree
+
+type Node<'a> = { Item: 'a; mutable Left: Node<'a> option; mutable Right: Node<'a> option; Parent: Node<'a> option }
+
+
+let rec add (parent: Node<'a> option) (node: Node<'a> option) (item: 'a) =
+    match node with
+    | None ->
+        match parent with
+        | Some parent when item < parent.Item -> 
+            parent.Left <- Some {Item = item; Left = None; Right = None; Parent = Some parent }
+        | Some parent when item > parent.Item ->
+            parent.Right <- Some {Item = item; Left = None; Right = None; Parent = Some parent }
+        | _ -> ()
+    | Some node when item < node.Item -> add (Some node) node.Left item
+    | Some node when item > node.Item -> add (Some node) node.Right item
+    | _ -> ()
+
+let rec find (node: Node<'a> option) (item: 'a) =
+    match node with
+    | Some node when item < node.Item -> find node.Left item
+    | Some node when item > node.Item -> find node.Right item
+    | Some node when item = node.Item -> Some node
+    | _ -> None
