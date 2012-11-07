140e91d6551ee4935d3629675404e7a40e0d47c0
Andrew Walker <alwalker21@gmail.com>
2012-11-06 21:38:44 -0600

    Added delete and inorder traversal to F# BST. Delete not finished.

--- BinarySearchTree/FSharp/Program.fs
+++ BinarySearchTree/FSharp/Program.fs
@@ -4,9 +4,21 @@
 module Program
 
 open System
+open Tree
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    //printfn "%A" argv
+    let root = {Item = 10; Left = None; Right = None; Parent = None}
+    add None (Some root) 5
+    add None (Some root) 4
+    add None (Some root) 6
+    add None (Some root) 15
+    add None (Some root) 14
+    add None (Some root) 16
+
+    delete (Some root) 16
+
+    printfn "%A" root
     Console.ReadLine() |> ignore
     0 // return an integer exit code
--- BinarySearchTree/FSharp/Tree.fs
+++ BinarySearchTree/FSharp/Tree.fs
@@ -1,6 +1,7 @@
 ﻿module Tree
 
-type Node<'a> = { Item: 'a; mutable Left: Node<'a> option; mutable Right: Node<'a> option; Parent: Node<'a> option }
+type Node<'a> = 
+    { mutable Item: 'a; mutable Left: Node<'a> option; mutable Right: Node<'a> option; mutable Parent: Node<'a> option }
 
 
 let rec add (parent: Node<'a> option) (node: Node<'a> option) (item: 'a) =
@@ -16,9 +17,45 @@ let rec add (parent: Node<'a> option) (node: Node<'a> option) (item: 'a) =
     | Some node when item > node.Item -> add (Some node) node.Right item
     | _ -> ()
 
+let rec delete (node: Node<'a> option) (item: 'a) =
+    match node with
+    | None -> ()
+    | Some node when item < node.Item -> delete node.Left item
+    | Some node when item > node.Item -> delete node.Right item
+    | Some node when item = node.Item && node.Left.IsSome && node.Right.IsSome ->
+        node.Item <- node.Left.Value.Item
+        delete node.Left node.Left.Value.Item
+    | Some node when item = node.Item && node.Left.IsNone && node.Right.IsNone ->
+        match node.Parent with
+        | None -> printf "You tried to delete root" //figure out how to do this
+        | x when x.Value.Left = Some node -> node.Parent.Value.Left <- None
+        | x when x.Value.Right = Some node -> node.Parent.Value.Right <- None
+        | _ -> ()
+    | Some node when item = node.Item && node.Left.IsNone && node.Right.IsSome ->
+        match node.Parent with
+        | None -> printf "You tried to delete the right node of root" //figure out how to do this
+        | parent when parent.Value.Left = Some node -> node.Parent.Value.Left <- node.Right
+        | parent when parent.Value.Right = Some node -> node.Parent.Value.Right <- node.Right
+        | _ -> ()
+    | Some node when item = node.Item && node.Left.IsSome && node.Right.IsNone ->
+        match node.Parent with
+        | None -> printf "You tried to delete the left node of root" //figure out how to do this
+        | parent when parent.Value.Left = Some node -> node.Parent.Value.Left <- node.Left
+        | parent when parent.Value.Right = Some node -> node.Parent.Value.Right <- node.Left
+        | _ -> ()
+    | _ -> ()
+
 let rec find (node: Node<'a> option) (item: 'a) =
     match node with
     | Some node when item < node.Item -> find node.Left item
     | Some node when item > node.Item -> find node.Right item
     | Some node when item = node.Item -> Some node
     | _ -> None
+
+let rec traverse(node: Node<'a> option) f =
+    match node with
+    | None -> ()
+    | Some node ->
+        traverse node.Left f
+        f node.Item
+        traverse node.Right f
